5cfc6c75f00c86d80d55d27cecb11ad77249cf7b
Mukesh Dangi <mukeshkdangi@users.noreply.github.com>
2019-01-30 00:05:34 -0800

    Add files via upload
    Video Player

---
+++ Image.java
@@ -0,0 +1,365 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.*;
+
+/**
+ * Created by poojit on 9/28/14.
+ */
+public class Image {
+    private int height;
+    private int width;
+    private int colorModel;
+    private byte[] a;
+    private byte[] r;
+    private byte[] g;
+    private byte[] b;
+    private int length;
+    private byte[] data;
+    private BufferedImage img;
+
+    public Image(String filename, int width, int height, int colorModel) {
+        this.width = width;
+        this.height = height;
+        this.colorModel = colorModel;
+        this.data = new byte[this.width * this.height * 3];
+        this.img = new BufferedImage(this.width, this.height, this.colorModel);
+        setBufferedImage(filename);
+    }
+
+    public Image(int width, int height, int colorModel) {
+        this.width = width;
+        this.height = height;
+        this.colorModel = colorModel;
+        this.data = new byte[this.width * this.height * 3];
+        this.img = new BufferedImage(this.width, this.height, this.colorModel);
+    }
+
+    public static void resize(Image src, Image dst, float scaleW, float scaleH, int anti_aliasing, int analysis) {
+        if (analysis == 0) {
+            if (anti_aliasing == 0) {
+                for (int y = 0; y < dst.height(); y++) {
+                    for (int x = 0; x < dst.width(); x++) {
+                        int[] rgb = src.getRGB(x,y);
+                        int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                        dst.setRGB(x, y, pix);
+                    }
+                }
+            } else {
+                for (int y = 0; y < dst.height(); y++) {
+                    for (int x = 0; x < dst.width(); x++) {
+                        int src_X = (int) Math.floor(x / scaleW);
+                        int src_Y = (int) Math.floor(y / scaleH);
+                        int[] averageXY = src.getAverageRGB(src_X, src_Y);
+                        int pix = 0xff000000 | ((averageXY[0] & 0xff) << 16) | ((averageXY[1] & 0xff) << 8) | (averageXY[2] & 0xff);
+                        dst.setRGB(x, y, pix);
+                    }
+                }
+            }
+        } else if (analysis == 1) {
+            if (anti_aliasing == 0) {
+                int resizeType = -1;
+                int squareSide = 0;
+                int side1 = 0;
+                int side2 = 0;
+                if (dst.height() > dst.width()) {
+                    resizeType = 0; // Vertical
+                    int squareSideSrc = src.width();
+                    squareSide = dst.width();
+                    float ratio = (float) squareSide / squareSideSrc;
+
+                    side1 = (int) Math.floor((dst.height() - squareSide) / 2.0f);
+                    side2 = (int) Math.ceil((dst.height() - squareSide) / 2.0f);
+
+                    int side1src = (int) Math.floor((src.height() - squareSideSrc) / 2.0f);
+                    int side2src = (int) Math.ceil((src.height() - squareSideSrc) / 2.0f);
+
+                    for (int y = 0; y < dst.height(); y++) {
+                        for (int x = 0; x < dst.width(); x++) {
+                            if (y < side1) {
+                                int[] rgb = src.getRGB((int) Math.floor(x / scaleW), (int) Math.floor(y / ((float) side1 / side1src)));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            } else if (y > (side1 + squareSide)) {
+                                float scale3 = (float) side2 / side2src;
+                                int[] rgb = src.getRGB((int) Math.floor(x / scaleW), (int) Math.floor((y - side1 - squareSide) / scale3 + side1src + squareSideSrc));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            } else {
+                                int[] rgb = src.getRGB((int) Math.floor(x / ratio), (int) Math.floor((y - side1 + side1src) / ratio));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            }
+                        }
+                    }
+                } else {
+                    resizeType = 1; // Horizontal
+                    int squareSideSrc = src.height();
+                    squareSide = dst.height();
+                    float ratio = (float) squareSide / squareSideSrc;
+
+                    side1 = (int) Math.floor((dst.width() - squareSide) / 2.0f);
+                    side2 = (int) Math.ceil((dst.width() - squareSide) / 2.0f);
+
+                    int side1src = (int) Math.floor((src.width() - squareSideSrc) / 2.0f);
+                    int side2src = (int) Math.ceil((src.width() - squareSideSrc) / 2.0f);
+
+                    for (int y = 0; y < dst.height(); y++) {
+                        for (int x = 0; x < dst.width(); x++) {
+                            if (x < side1) {
+                                int[] rgb = src.getRGB((int) Math.floor(x / ((float) side1 / side1src)), (int) Math.floor(y / scaleH));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            } else if (x > (side1 + squareSide)) {
+                                float scale3 = (float) side2 / side2src;
+                                int[] rgb = src.getRGB((int) Math.floor((x - side1 - squareSide) / scale3 + side1src + squareSideSrc), (int) Math.floor(y / scaleH));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            } else {
+                                int[] rgb = src.getRGB((int) Math.floor((x - side1 + side1src) / ratio), (int) Math.floor(y / ratio));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            }
+
+                        }
+                    }
+                }
+            } else {
+                int resizeType = -1;
+                int squareSide = 0;
+                int side1 = 0;
+                int side2 = 0;
+                if (dst.height() > dst.width()) {
+                    resizeType = 0; // Vertical
+                    int squareSideSrc = src.width();
+                    squareSide = dst.width();
+                    float ratio = (float) squareSide / squareSideSrc;
+
+                    side1 = (int) Math.floor((dst.height() - squareSide) / 2.0f);
+                    side2 = (int) Math.ceil((dst.height() - squareSide) / 2.0f);
+
+                    int side1src = (int) Math.floor((src.height() - squareSideSrc) / 2.0f);
+                    int side2src = (int) Math.ceil((src.height() - squareSideSrc) / 2.0f);
+
+                    for (int y = 0; y < dst.height(); y++) {
+                        for (int x = 0; x < dst.width(); x++) {
+                            if (y < side1) {
+                                int[] rgb = src.getAverageRGB((int) Math.floor(x / scaleW), (int) Math.floor(y / ((float) side1 / side1src)));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            } else if (y > (side1 + squareSide)) {
+                                float scale3 = (float) side2 / side2src;
+                                int[] rgb = src.getAverageRGB((int) Math.floor(x / scaleW), (int) Math.floor((y - side1 - squareSide) / scale3 + side1src + squareSideSrc));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            } else {
+                                int[] rgb = src.getAverageRGB((int) Math.floor(x / ratio), (int) Math.floor((y - side1 + side1src) / ratio));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            }
+                        }
+                    }
+                } else {
+                    resizeType = 1; // Horizontal
+                    int squareSideSrc = src.height();
+                    squareSide = dst.height();
+                    float ratio = (float) squareSide / squareSideSrc;
+
+                    side1 = (int) Math.floor((dst.width() - squareSide) / 2.0f);
+                    side2 = (int) Math.ceil((dst.width() - squareSide) / 2.0f);
+
+                    int side1src = (int) Math.floor((src.width() - squareSideSrc) / 2.0f);
+                    int side2src = (int) Math.ceil((src.width() - squareSideSrc) / 2.0f);
+
+                    for (int y = 0; y < dst.height(); y++) {
+                        for (int x = 0; x < dst.width(); x++) {
+                            if (x < side1) {
+                                int[] rgb = src.getAverageRGB((int) Math.floor(x / ((float) side1 / side1src)), (int) Math.floor(y / scaleH));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            } else if (x > (side1 + squareSide)) {
+                                float scale3 = (float) side2 / side2src;
+                                int[] rgb = src.getAverageRGB((int) Math.floor((x - side1 - squareSide) / scale3 + side1src + squareSideSrc), (int) Math.floor(y / scaleH));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            } else {
+                                int[] rgb = src.getAverageRGB((int) Math.floor((x - side1 + side1src) / ratio), (int) Math.floor(y / ratio));
+                                int pix = 0xff000000 | ((rgb[0] & 0xff) << 16) | ((rgb[1] & 0xff) << 8) | (rgb[2] & 0xff);
+                                dst.setRGB(x, y, pix);
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private int[] getAverageRGB(int x, int y) {
+        float[] rgb = new float[3];
+        rgb[0] = 0;
+        rgb[1] = 0;
+        rgb[2] = 0;
+        if (x > 0 && y > 0 && x < this.width - 1 && y < this.height - 1) {
+            for (int i = x - 1; i <= x + 1; i++) {
+                for (int j = y - 1; j <= y + 1; j++) {
+                    int[] points = this.getRGB(i, j);
+                    for (int c = 0; c < 3; c++) {
+                        rgb[c] += points[c] / 9.0f;
+                    }
+                }
+            }
+        } else if (x == 0 && y == 0) {
+            for (int i = 0; i <= 1; i++) {
+                for (int j = 0; j <= 1; j++) {
+                    int[] points = this.getRGB(i, j);
+                    for (int c = 0; c < 3; c++) {
+                        rgb[c] += points[c] / 4.0f;
+                    }
+                }
+            }
+        } else if (x == 0 && y == this.height - 1) {
+            for (int i = 0; i <= 1; i++) {
+                for (int j = y - 1; j <= y; j++) {
+                    int[] points = this.getRGB(i, j);
+                    for (int c = 0; c < 3; c++) {
+                        rgb[c] += points[c] / 4.0f;
+                    }
+                }
+            }
+        } else if (x == this.width - 1 && y == 0) {
+            for (int i = x - 1; i <= x; i++) {
+                for (int j = 0; j <= 1; j++) {
+                    int[] points = this.getRGB(i, j);
+                    for (int c = 0; c < 3; c++) {
+                        rgb[c] += points[c] / 4.0f;
+                    }
+                }
+            }
+        } else if (x == this.width - 1 && y == this.height - 1) {
+            for (int i = x - 1; i <= x; i++) {
+                for (int j = y - 1; j <= y; j++) {
+                    int[] points = this.getRGB(i, j);
+                    for (int c = 0; c < 3; c++) {
+                        rgb[c] += points[c] / 4.0f;
+                    }
+                }
+            }
+        } else if (x == 0) {
+            for (int i = 0; i <= 1; i++) {
+                for (int j = y - 1; j <= y + 1; j++) {
+                    int[] points = this.getRGB(i, j);
+                    for (int c = 0; c < 3; c++) {
+                        rgb[c] += points[c] / 6.0f;
+                    }
+                }
+            }
+        } else if (y == 0) {
+            for (int i = x - 1; i <= x + 1; i++) {
+                for (int j = 0; j <= 1; j++) {
+                    int[] points = this.getRGB(i, j);
+                    for (int c = 0; c < 3; c++) {
+                        rgb[c] += points[c] / 6.0f;
+                    }
+                }
+            }
+        } else if (x == this.width - 1) {
+            for (int i = x - 1; i <= x; i++) {
+                for (int j = y - 1; j <= y + 1; j++) {
+                    int[] points = this.getRGB(i, j);
+                    for (int c = 0; c < 3; c++) {
+                        rgb[c] += points[c] / 6.0f;
+                    }
+                }
+            }
+        } else if (y == this.height - 1) {
+            for (int i = x - 1; i <= x + 1; i++) {
+                for (int j = y - 1; j <= y; j++) {
+                    int[] points = this.getRGB(i, j);
+                    for (int c = 0; c < 3; c++) {
+                        rgb[c] += points[c] / 6.0f;
+                    }
+                }
+            }
+        }
+        int[] rgbInt = new int[3];
+        rgbInt[0] = (int) Math.floor(rgb[0]);
+        rgbInt[1] = (int) Math.floor(rgb[1]);
+        rgbInt[2] = (int) Math.floor(rgb[2]);
+        return rgbInt;
+    }
+
+    public BufferedImage clone() {
+        WritableRaster raster = this.img.copyData(null);
+        return new BufferedImage(this.img.getColorModel(), raster, false, null);
+    }
+
+    private void getDataFromFile(String filename) {
+        try {
+            File file = new File(filename);
+            InputStream is = new FileInputStream(file);
+
+            this.length = (int) file.length();
+
+            int offset = 0;
+            int numRead = 0;
+            while (offset < this.length && (numRead = is.read(this.data, offset, this.length - offset)) >= 0) {
+                offset += numRead;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setBufferedImage(byte[] data) {
+        this.data = data;
+        int idx = 0;
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.width; x++) {
+                byte a = 0;
+                byte r = this.data[idx];
+                byte g = this.data[idx + this.height * this.width];
+                byte b = this.data[idx + this.height * (this.width << 1)];
+
+                int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+                this.img.setRGB(x, y, pix);
+                idx++;
+            }
+        }
+    }
+
+    public void setBufferedImage(String filename) {
+        getDataFromFile(filename);
+        setBufferedImage(this.data);
+    }
+
+    public BufferedImage getImg() {
+        return this.img;
+    }
+
+    public int height() {
+        return this.height;
+    }
+
+    public int width() {
+        return this.width;
+    }
+
+    public void setRGB(int x, int y, int pix) {
+        this.img.setRGB(x, y, pix);
+    }
+
+    public int[] getRGB(int x, int y) {
+        int xx = x;
+        int yy = y;
+        if (x >= this.width) xx--;
+        if (y >= this.height) yy--;
+        int[] rgb = new int[3];
+        int pix = this.img.getRGB(xx, yy);
+        rgb[0] = (pix >> 16) & 0xff;
+        rgb[1] = (pix >> 8) & 0xff;
+        rgb[2] = (pix & 0xff);
+        return rgb;
+    }
+}
--- ImageDisplay.java
+++ ImageDisplay.java
@@ -2,8 +2,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
@@ -15,12 +13,16 @@ public class ImageDisplay {
     private JFrame frame;
     private JLabel lbIm1;
     private JLabel lbIm2;
-    private BufferedImage img;
-    private InputStream is;
+    private BufferedImage image;
+    private InputStream inputStream;
+    public static int RGB_FACTOR = 3;
+
     private int width = 960;
     private int height = 540;
+
     private int width_o = width;
     private int height_o = height;
+
     double factor_w = 1;
     double factor_h = 1;
 
@@ -35,17 +37,14 @@ public class ImageDisplay {
 
         long fps = Long.parseLong(args[3]);
 
-        // Use labels to display the images
         frame = new JFrame();
         GridBagLayout gLayout = new GridBagLayout();
         frame.getContentPane().setLayout(gLayout);
+
         JLabel lbText1 = new JLabel("Video height" + height + " width" + width);
+
         lbText1.setHorizontalAlignment(SwingConstants.CENTER);
-
-        //Creation of array list for obtaining pixel values.
         ArrayList<ImageIcon> images = new ArrayList<ImageIcon>();
-
-        //Generation of the GUI for display.
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = GridBagConstraints.CENTER;
@@ -60,50 +59,52 @@ public class ImageDisplay {
         // Taking input parameters for display.
         try {
             File file = new File(args[0]);
-            is = new FileInputStream(file);
+            inputStream = new FileInputStream(file);
 
             long len = file.length();
             byte[] bytes = new byte[(int) len];
 
             int offset = 0;
             int numRead = 0;
-            while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+            while (offset < bytes.length && (numRead = inputStream.read(bytes, offset, bytes.length - offset)) >= 0) {
                 offset += numRead;
             }
 
-            // Processing code begins here.
-            img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
             int count = 0;
             while (count == 0) {
                 count++;
-                System.out.println();
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                for (int i = 0; i < (len / (3.0 * width * width)); i++ ) {
+                System.out.println();
+                int numberOfFrames = (int) (len / (RGB_FACTOR * width * width));
 
+                for (int index = 0; index < (numberOfFrames); index++) {
                     long lStartTime = System.currentTimeMillis();
 
-                    int ind = (int) ((3 * width * height * i));
-                    for (int y = 0; y < height; y++) {
+                    int ind = (int) ((RGB_FACTOR * width_o * height_o * index));
+                    for (int yAxis = 0; yAxis < height; yAxis++) {
+                        for (int xAxis = 0; xAxis < width; xAxis++) {
 
-                        for (int x = 0; x < width; x++) {
-                            byte r = bytes[ind];
-                            byte g = bytes[ind + height_o * width_o];
-                            byte b = bytes[ind + height_o * width_o * 2];
+                            byte red = bytes[ind];
+                            //Shift on ce length to get green array
+                            byte green = bytes[ind + height_o * width_o];
+                            //Shift twice length to get blue array
+                            byte blue = bytes[ind + height_o * width_o * 2];
 
-                            int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
-                            img.setRGB(x, y, pix);// set the value to the location.
-                            ind++;
+                            int pixValue = 0xff000000 | ((red & 0xff) << 16) | ((green & 0xff) << 8) | (blue & 0xff);
+                            image.setRGB(xAxis, yAxis, pixValue);
+                            ind = ind + 2;
 
                         }
                     }
-                    ImageIcon image = new ImageIcon(img);
-                    images.add(image);// Addition of pixel values to the frames created.
-                    long lEndTime = System.currentTimeMillis();// Calculation of the amount of time taken for processing inside for loop.
-                    long output = lEndTime - lStartTime;
-                    displayFrame(image, gridBagConstraints);// Display per frame.
+
+                    ImageIcon image = new ImageIcon(this.image);
+                    images.add(image);
+                    long lEndTime = System.currentTimeMillis();
+                    displayFrame(image, gridBagConstraints);
 
                     try {
-                        long delay = (long) ((1000.0 / fps) - output);
+                        long delay = (long) ((1000.0 / fps) - (lEndTime - lStartTime));
                         Thread.sleep((delay > 0) ? delay : 0);
                     } catch (InterruptedException ex) {
                         Thread.currentThread().interrupt();
@@ -112,9 +113,7 @@ public class ImageDisplay {
                 }
             }
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
